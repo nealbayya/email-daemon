59c28e4549946747cef7b25b75fb379f2cf2b224
Neal Bayya <neal.bayya@gmail.com>
2024-11-02 19:29:15 -0700

    remove unnecessary file attachments

--- reporter.py
+++ reporter.py
@@ -11,11 +11,9 @@ SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 TO_ADDRESS = os.environ.get('TO_ADDRESS')
-SUBJECT = 'Daily Report'
-BODY = 'Please find attached the daily report.'
+SUBJECT = 'Weekly report'
+BODY = 'TODO'
 
-# File to send
-ATTACHMENT_PATH = 'path/to/your/report.pdf'
 
 def send_email():
     # Create a multipart message
@@ -26,14 +24,6 @@ def send_email():
 
     # Attach the body with the msg instance
     msg.attach(MIMEText(BODY, 'plain'))
-
-    # Open the file to be sent
-    with open(ATTACHMENT_PATH, 'rb') as attachment:
-        mime_base = MIMEBase('application', 'octet-stream')
-        mime_base.set_payload(attachment.read())
-        encoders.encode_base64(mime_base)
-        mime_base.add_header('Content-Disposition', f'attachment; filename={os.path.basename(ATTACHMENT_PATH)}')
-        msg.attach(mime_base)
 
     # Create SMTP session for sending the mail
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
